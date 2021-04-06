661c28865b59d6434ecc59ef30ac6ddfa7142302
Lachlan Heywood <lachlan@heywood.net.au>
2021-04-06 15:49:40 -0400

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Container>
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
         </Switch>
-      </div>
+      </Container>
     </Router>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,63 @@
+import { useState } from 'react';
+import {
+  Button,
+  Col,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from 'react-bootstrap';
+import MovieCard from '../components/MovieCard';
+
 export default function Home() {
+  const [search, setSearch] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchMovies();
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const fetchMovies = () => {
+    fetch(`http://www.omdbapi.com/?apikey=59354c85&s=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.Search || []);
+      });
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Scene-It</h1>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Search Movies"
+            aria-label="Search Movies"
+            onChange={handleChange}
+            value={search}
+          />
+          <InputGroup.Append>
+            <Button type="submit" variant="outline-secondary">
+              Search
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form>
+
+      <Row>
+        {movies.map((movie) => {
+          return (
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+              <MovieCard movie={movie} />
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
